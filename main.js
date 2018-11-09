b8d8bf9d79047366cef3a67b948546e3c160df11
ArseniiT <arseniit@gmail.com>
2018-11-08 23:50:01 -0500

    changing all the colors of the castle

--- main.js
+++ main.js
@@ -1,59 +1,32 @@
 
 // here is classes and methods which use Duke Learn To Programming library (DLTP)
 
-// write your code here
+// **************** 1 *********************
 let x = 10;
 let y = 2;
 let z = x*y;
 print(z);
 
 
-
+// **************** 2 *********************
 class Animal{
     constructor(name){
         this.name = name;
     }
-    
     printName(){
         print('My name is ' + this.name);
     }
 }
-
 let cat = new Animal('Kitty');
-
 cat.printName();
 
-let img = new SimpleImage('chapel.png');
-print(img);
-print(img.getWidth());
-print(img.getHeight());
-//print(img.getPixel());
-
-function imgSize(img){
-    print(img.getWidth() + ' x ' + img.getHeight());
-}
-
-imgSize(img);
-
+// **************** 3 *********************
 function textConcat(t1, t2){
     print(t1 + ' ' + t2);
 }
-
 textConcat('text one', 'and text two.');
 
-// changing sky blue beside of the castle
-for(let pixel of img.values()) {
-    
-    if(pixel.getBlue()>160 &&(pixel.getGreen()<250) && (pixel.getRed()<200)){
-        pixel.setBlue(0);
-        pixel.setGreen(0);
-        pixel.setRed(0);
-    }
-    
-}
-
-print(img);
-
+// **************** 4 *********************
 // yellow square
 let img2 = new SimpleImage(200, 200);
 
@@ -62,5 +35,32 @@ for(let pixel of img2.values()) {
     pixel.setGreen(255);
     pixel.setBlue(0);
 }
-
 print(img2);
+
+// **************** 5 *********************
+// original image of castle
+let img = new SimpleImage('chapel.png');
+print(img);
+
+function imgSize(img){
+    print(img.getWidth() + ' x ' + img.getHeight());
+}
+imgSize(img);
+
+// **************** 6 *********************
+
+for(let pixel of img.values()) {
+// changing all the colors of the castle
+    pixel.setRed(pixel.getGreen());
+    pixel.setGreen(pixel.getBlue());
+    pixel.setBlue(pixel.getRed());
+
+// changing sky blue beside of the castle    
+    /*if(pixel.getBlue()>160 &&(pixel.getGreen()<250) && (pixel.getRed()<200)){
+        pixel.setBlue(0);
+        pixel.setGreen(0);
+        pixel.setRed(0);
+    }*/
+}
+print(img);
+
