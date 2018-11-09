30ffe8b1801261de552284f749daa0ef897dda1e
ArseniiT <arseniit@gmail.com>
2018-11-09 02:21:45 -0500

    bug fix. find and fix the bug (yellow has to be red)

--- main.js
+++ main.js
@@ -2,27 +2,27 @@
 // here is classes and methods which use Duke Learn To Programming library (DLTP)
 
 // **************** 1 *********************
-let x = 10;
+/*let x = 10;
 let y = 2;
 let z = x*y;
-print(z);
+print(z);*/
 
 
 // **************** 2 *********************
 class Animal{
-    constructor(name){
-        this.name = name;
-    }
-    printName(){
-        print('My name is ' + this.name);
-    }
+  constructor(name){
+      this.name = name;
+  }
+  printName(){
+      print('My name is ' + this.name);
+  }
 }
 let cat = new Animal('Kitty');
 cat.printName();
 
 // **************** 3 *********************
 function textConcat(t1, t2){
-    print(t1 + ' ' + t2);
+  print(t1 + ' ' + t2);
 }
 textConcat('text one', 'and text two.');
 
@@ -31,9 +31,9 @@ textConcat('text one', 'and text two.');
 let img2 = new SimpleImage(200, 200);
 
 for(let pixel of img2.values()) {
-    pixel.setRed(255);
-    pixel.setGreen(255);
-    pixel.setBlue(0);
+  pixel.setRed(255);
+  pixel.setGreen(255);
+  pixel.setBlue(0);
 }
 print(img2);
 
@@ -43,7 +43,7 @@ let img = new SimpleImage('chapel.png');
 print(img);
 
 function imgSize(img){
-    print(img.getWidth() + ' x ' + img.getHeight());
+  print(img.getWidth() + ' x ' + img.getHeight());
 }
 imgSize(img);
 
@@ -51,16 +51,16 @@ imgSize(img);
 
 for(let pixel of img.values()) {
 // changing all the colors of the chapel
-    pixel.setRed(pixel.getGreen());
-    pixel.setGreen(pixel.getBlue());
-    pixel.setBlue(pixel.getRed());
+  pixel.setRed(pixel.getGreen());
+  pixel.setGreen(pixel.getBlue());
+  pixel.setBlue(pixel.getRed());
 
 // changing sky blue beside of the chapel    
-    /*if(pixel.getBlue()>160 &&(pixel.getGreen()<250) && (pixel.getRed()<200)){
-        pixel.setBlue(0);
-        pixel.setGreen(0);
-        pixel.setRed(0);
-    }*/
+  /*if(pixel.getBlue()>160 &&(pixel.getGreen()<250) && (pixel.getRed()<200)){
+      pixel.setBlue(0);
+      pixel.setGreen(0);
+      pixel.setRed(0);
+  }*/
 }
 print(img);
 
@@ -70,26 +70,26 @@ let photo = new SimpleImage('hilton.jpg');
 let photoW = photo.getWidth();
 print(photo);
 for(let pixel of photo.values()) {
-    if(pixel.getX()<photoW/3){
-        pixel.setRed(255);
-    } else if(pixel.getX()>photoW/3 && pixel.getX()>(photoW*2/3) ) {
-        pixel.setBlue(255);
-    } else {
-        pixel.setGreen(255);
-    }
+  if(pixel.getX()<photoW/3){
+      pixel.setRed(255);
+  } else if(pixel.getX()>photoW/3 && pixel.getX()>(photoW*2/3) ) {
+      pixel.setBlue(255);
+  } else {
+      pixel.setGreen(255);
+  }
 }
 print(photo);
 
 // **************** 8 *********************
 //  swap the red and green values of the pixel
 function swapRedGreen(pixel) {
-    let tmp = pixel.getRed();
-    pixel.setRed(pixel.getGreen());
-    pixel.setGreen(tmp);
+  let tmp = pixel.getRed();
+  pixel.setRed(pixel.getGreen());
+  pixel.setGreen(tmp);
 }
 
 for(let pixel of photo.values()) {
-    swapRedGreen(pixel);
+  swapRedGreen(pixel);
 }
 print(photo);
 
@@ -99,12 +99,12 @@ let dukeDevil = new SimpleImage('duke_blue_devil.png');
 print(dukeDevil);
 
 for(let pixel of dukeDevil.values()) {
-    // all except white color
-    if(pixel.getBlue() !=255 && pixel.getRed() !=255 && pixel.getGreen() !=255){
-        pixel.setRed(255);
-        pixel.setGreen(255);
-        pixel.setBlue(0);
-    }
+  // all except white color
+  if(pixel.getBlue() !=255 && pixel.getRed() !=255 && pixel.getGreen() !=255){
+      pixel.setRed(255);
+      pixel.setGreen(255);
+      pixel.setBlue(0);
+  }
 }
 print(dukeDevil);
 
@@ -115,11 +115,30 @@ let dino = new SimpleImage('dinos.png');
 
 print(rob);
 for(let pixel of rob.values()) {
-  //if green is more common than red and blue together
-    if(pixel.getGreen() > (pixel.getRed() + pixel.getBlue())){
-        let x = pixel.getX();
-        let y = pixel.getY();
-        pixel.setAllFrom(dino.getPixel(x,y));
-    } 
+//if green is more common than red and blue together
+  if(pixel.getGreen() > (pixel.getRed() + pixel.getBlue())){
+      let x = pixel.getX();
+      let y = pixel.getY();
+      pixel.setAllFrom(dino.getPixel(x,y));
+  } 
 }
 print(rob);
+
+// **************** 11 *********************
+//  find and fix the bug (yellow has to be red)
+var imgM = new SimpleImage(200,200);
+for (var px of imgM.values()){
+var x = px.getX();
+var y = px.getY();
+if (x < imgM.getWidth()/2){
+  px.setRed(255);
+}
+if (y>imgM.getHeight()/2){
+  px.setBlue(255);
+}
+// bug was here
+if (y<=imgM.getHeight()/2 && x >= imgM.getWidth()/2) {
+  px.setGreen(255);
+}
+}
+print (imgM);
