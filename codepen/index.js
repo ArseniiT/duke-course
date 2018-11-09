460220cecad49e68462daee9ce5b243127881403
ArseniiT <arseniit@gmail.com>
2018-11-09 15:30:31 -0500

    rebuild progect

---
+++ codepen/index.js
@@ -0,0 +1,9 @@
+function doChange() {
+  let d1 = document.getElementById('yellow-txt');
+  let d2 = document.getElementById('blue-txt');
+  d1.className = '';
+  d2.className = '';
+}
+function divClick() {
+  alert("It's not a button");
+}
