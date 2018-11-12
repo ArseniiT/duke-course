441cd3198b9cda4144966958a3753093cb779e74
ArseniiT <arseniit@gmail.com>
2018-11-12 03:20:21 -0500

    realized function for combine images with using green screen

--- week3/main.js
+++ week3/main.js
@@ -1,15 +1,18 @@
 let canvas1;
 let canvas2;
+let canvas3;
 let fgImage = null;
 let bgImage = null;
+let greenThreshold = 200;
 
 window.addEventListener('load', ()=>{
   canvas1 = document.querySelector('#canvas1');
   canvas2 = document.querySelector('#canvas2');
+  canvas3 = document.querySelector('#canvas3');
   //let context1 = canvas1.getContext('2d');
   //let context2 = canvas2.getContext('2d');
-  fgImage = document.getElementById('fgfile');
-  bgImage = document.getElementById('bgfile');
+  //fgImage = document.getElementById('fgfile');
+  //bgImage = document.getElementById('bgfile');
 
 });
 
@@ -33,5 +36,19 @@ function greenScreen() {
   if(bgImage == null || !bgImage.complete()) {
     alert('Background not loaded.');
   }
-  clearCanvas();
+  //clearCanvas();
+
+  let output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+  for(let pixel of fgImage.values()) {
+    let x = pixel.getX();
+    let y = pixel.getY();
+    if (pixel.getGreen() > greenThreshold) {
+      let bgPixel = bgImage.getPixel(x, y);
+      output.setPixel(x, y, bgPixel);
+    } else {
+      output.setPixel(x, y, pixel);
+    }
+  }
+
+  output.drawTo(canvas3);
 }
