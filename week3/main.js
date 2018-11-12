d33428772ddc424624d8ce7c96482e100d95336f
ArseniiT <arseniit@gmail.com>
2018-11-12 02:11:38 -0500

    made two loading functions in two canvases

--- week3/main.js
+++ week3/main.js
@@ -1,13 +1,37 @@
-window.addEventListener('load', ()=>{
-  let canvas1 = document.querySelector('#canvas1');
-  let canvas2 = document.querySelector('#canvas2');
-  let context1 = canvas1.getContext('2d');
-  let context2 = canvas2.getContext('2d');
+let canvas1;
+let canvas2;
+let fgImage = null;
+let bgImage = null;
 
-  context1.fillStyle='green';
-  context2.fillStyle='red';
-  
-  context1.fillRect(0,0, canvas1.clientWidth, canvas1.height);
-  context2.fillRect(0,0, canvas2.clientWidth, canvas2.height);
-  
+window.addEventListener('load', ()=>{
+  canvas1 = document.querySelector('#canvas1');
+  canvas2 = document.querySelector('#canvas2');
+  //let context1 = canvas1.getContext('2d');
+  //let context2 = canvas2.getContext('2d');
+  fgImage = document.getElementById('fgfile');
+  bgImage = document.getElementById('bgfile');
+
 });
+
+function loadForegroundImage() {
+  let imgFile = document.getElementById('fgfile');
+  fgImage = new SimpleImage(imgFile);
+  fgImage.drawTo(canvas1);
+}
+
+function loadBackgroundImage() {
+  let imgFile = document.getElementById('bgfile');
+  bgImage = new SimpleImage(imgFile);
+  bgImage.drawTo(canvas2);
+}
+
+function greenScreen() {
+  if(fgImage == null || !fgImage.complete()) {
+    alert('Foreground not loaded.');
+    return;
+  }
+  if(bgImage == null || !bgImage.complete()) {
+    alert('Background not loaded.');
+  }
+  clearCanvas();
+}
